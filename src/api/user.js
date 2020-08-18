1560add71432383d7d54ee37113985d0174e95d6
lixiaoqiang1 <361028458@qq.com>
2020-08-18 16:11:22 +0800

    add

---
+++ src/api/user.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+// 用户获取验证码
+export function need_code(data) {
+  return request({
+    url: '/need_code',
+    method: 'post',
+    data
+  })
+}
+// 验证码
+export function sendCode(data) {
+  return request({
+    url: '/send_code',
+    method: 'post',
+    data
+  })
+}
+export function login(data) {
+  return request({
+    url: '/auth',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/api/v1/userInfo',
+    method: 'get'
+    // params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/logout',
+    method: 'GET'
+  })
+}
---
+++ src/store/modules/permission.js
@@ -0,0 +1,74 @@
+import { asyncRoutes, constantRoutes } from '@/router'
+
+/**
+ * Use meta.role to determine if the current user has permission
+ * @param roles
+ * @param route
+ */
+function hasPermission(permissions, route) {
+  console.log(route)
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+  // if (route.meta !== undefined && route.meta.icon !== undefined) {
+  //   if (route.children) {
+  //     for (const item of route.children) {
+  //       if (permissions.includes(item.path)) {
+  //         return true
+  //       }
+  //     }
+  //   }
+  //   return permissions.includes(route.path)
+  // }
+  return true
+}
+
+/**
+ * Filter asynchronous routing tables by recursion
+ * @param routes asyncRoutes
+ * @param roles
+ */
+export function filterAsyncRoutes(asyncRoutes, permissions) {
+  const res = []
+  asyncRoutes.forEach(route => {
+    const tmp = { ...route }
+    if (hasPermission(permissions, tmp)) {
+      if (tmp.children) {
+        tmp.children = filterAsyncRoutes(tmp.children, permissions)
+      }
+      res.push(tmp)
+    }
+  })
+  return res
+}
+
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+  generateRoutes({ commit }, permissions) {
+    return new Promise(resolve => {
+      const accessedRoutes = filterAsyncRoutes(asyncRoutes, permissions)
+      commit('SET_ROUTES', accessedRoutes)
+      resolve(accessedRoutes)
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
